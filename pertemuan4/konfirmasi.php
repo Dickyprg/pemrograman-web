415e00f69e891e6a9ad3988af164fd906e89ce0c
dickyprg <dickyprg14@gmail.com>
2020-10-06 14:11:35 +0700

    menambahkan form registrasi

---
+++ pertemuan4/konfirmasi.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['daftar'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $namadepan = $_POST['namadepan'];
+    $namabelakang = $_POST['namabelakang'];
+    $email = $_POST['email'];
+    
+    echo $username;
+    echo"<br>";
+    echo $email;
+    echo"<br>";
+    echo $namadepan." ".$namabelakang;
+}
+?>
