fd8e91276ecd2ded8d9a255fa5567fa136238ab1
dickyprg <dickyprg14@gmail.com>
2020-12-01 00:08:46 +0700

    pertemuan10

--- pertemuan9/app/Controllers/Home.php
+++ pertemuan9/app/Controllers/Home.php
@@ -4,7 +4,18 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('home_dashboard');
-		//echo "Ini adalah Controller HOME";
+		return view('Login_index');
 	}
+	public function dashboard($id,$nama)
+	{
+		$data = [
+			'title' => 'Halaman Dashboard',
+			'id' => $id,
+			'nama' => $nama
+		];
+		return view('home_dashboard');
+	}
+
+	//--------------------------------------------------------------------
+
 }
